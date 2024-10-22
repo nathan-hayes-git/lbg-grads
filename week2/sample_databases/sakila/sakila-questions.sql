2901ce7264975dd0cf3363ba991d7d0314ebe0d0
Nathan <Nathan.Hayes@lloydsbanking.com>
2024-10-22 14:55:37 +0100

    UPDATE Qs 6 - 10

--- week2/sample_databases/sakila/sakila-questions.sql
+++ week2/sample_databases/sakila/sakila-questions.sql
@@ -24,3 +24,32 @@ SELECT description
 FROM film
 WHERE film_id = 100;
 
+-- Q6
+SELECT *
+FROM film
+WHERE rating = "R";
+
+-- Q7
+SELECT *
+FROM film
+WHERE rating != "R";
+
+-- Q8
+SELECT *
+FROM film
+ORDER BY length ASC
+LIMIT 10;
+
+-- Q9
+SELECT *
+FROM film
+WHERE length = (
+	SELECT MAX(length) AS duration
+	FROM film
+);
+
+-- Q10
+SELECT *
+FROM film
+WHERE special_features LIKE "%Deleted Scenes%";
+
