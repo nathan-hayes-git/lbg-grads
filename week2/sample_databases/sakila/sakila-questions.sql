1e0bd73f02b3e9fbe0fa5219c9c1913b63843aa3
Nathan <Nathan.Hayes@lloydsbanking.com>
2024-10-22 14:43:13 +0100

    UPDATE Qs 1 - 5

--- week2/sample_databases/sakila/sakila-questions.sql
+++ week2/sample_databases/sakila/sakila-questions.sql
@@ -1,4 +1,26 @@
 USE sakila;
 
-SELECT COUNT(*)
-FROM film;
+-- Q1
+SELECT *
+FROM actor;
+
+-- Q2
+SELECT *
+FROM actor
+WHERE first_name = "John";
+
+-- Q3
+SELECT *
+FROM actor
+WHERE last_name = "Neeson";
+
+-- Q4
+SELECT *
+FROM actor
+WHERE actor_id MOD 10 = 0;
+
+-- Q5
+SELECT description
+FROM film
+WHERE film_id = 100;
+
